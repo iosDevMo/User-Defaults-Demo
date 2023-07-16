b2ff0b91923c4f2e2866c1bd527a9feab2f6f9b6
mohamdan <engineerm86@gmail.com>
2023-07-16 13:49:23 +0300

    finish

--- User-Defaults-Demo/User-Defaults-Demo/ViewController.swift
+++ User-Defaults-Demo/User-Defaults-Demo/ViewController.swift
@@ -14,15 +14,31 @@ class ViewController: UIViewController {
     @IBOutlet weak var speakerSegment: UISegmentedControl!
     @IBOutlet weak var modeSegment: UISegmentedControl!
     
+    var defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        muteSwitch.isUserInteractionEnabled = defaults.bool(forKey: "muteSwitch")
+        levelSlider.value = Float(defaults.double(forKey: "levelSlider"))
+        speakerSegment.selectedSegmentIndex = defaults.integer(forKey: "speakerSegment")
+        modeSegment.selectedSegmentIndex = defaults.integer(forKey: "modeSegment")
     }
 
-    @IBAction func modeSelect(_ sender: Any) {
+    
+    @IBAction func modeChanged(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex==0{
+            view.backgroundColor = UIColor(red: 0, green: 0, blue: 1, alpha: 1)
+        }else{
+            view.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+        
     }
     
     @IBAction func savePressed(_ sender: UIButton) {
+        defaults.set(muteSwitch.isUserInteractionEnabled, forKey: "muteSwitch")
+        defaults.set(levelSlider.value, forKey: "levelSlider")
+        defaults.set(speakerSegment.selectedSegmentIndex, forKey: "speakerSegment")
+        defaults.set(modeSegment.selectedSegmentIndex, forKey: "modeSegment")
     }
 }
 
